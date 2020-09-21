896c0e5ef71f1de4165e05407c920f42f9ec4254
Faysal Bulhan <fbbulhan@gmail.com>
2020-09-21 13:40:00 +0100

    Travis files added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alhamdulilah!
+          Salams to all!
         </p>
         <a
           className="App-link"
