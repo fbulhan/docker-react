5b7ca3969fb63f726608cca5a87aa47f164c1393
Faysal Bulhan <fbbulhan@gmail.com>
2020-09-22 08:18:24 +0100

    Changes to travis files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Salams to all!
+          Alhamdulilah for everything!
         </p>
         <a
           className="App-link"
